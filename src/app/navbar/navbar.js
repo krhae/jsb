1611b9a736922a536d27dd5d5ef5a9c96fcf007e
Kelsey Hawley <kelseyr.hawley@gmail.com>
2015-06-17 23:14:16 -0700

    Initiating the navbar code

---
+++ src/app/navbar/navbar.js
@@ -0,0 +1,16 @@
+/*
+ * NavBar
+ * Created: 17 June 2015
+ * Author: Kelsey Hawley
+ */
+
+ angular.module( 'jsb.navbar', [
+
+])
+
+.config(function config( $stateProvider ) {
+
+})
+
+.controller( 'navbarController', function NavbarController( $scope ) {
+});
