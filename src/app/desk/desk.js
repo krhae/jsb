0418254790351e57281a25a33cf50a413978a277
Kelsey Hawley <kelseyr.hawley@gmail.com>
2015-06-17 23:13:29 -0700

    Initiating the desk related code

---
+++ src/app/desk/desk.js
@@ -0,0 +1,24 @@
+/*
+ * Desk
+ * Created: 17 June 2015
+ * Author: Kelsey Hawley
+ */
+
+ angular.module( 'jsb.desk', [
+
+])
+
+.config(function config( $stateProvider ) {
+
+})
+
+.controller( 'deskController', function DeskController( $scope ) {
+
+    /**
+     * Set up the desktop canvas for use
+     * 
+     */
+    $scope.onLoad = function() {
+        
+    };
+});
