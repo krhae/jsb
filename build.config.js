40d2872588f67698510af01faed2a087b0c74b2f
Kelsey Hawley <kelseyr.hawley@gmail.com>
2015-06-17 23:14:47 -0700

    Adding angular-templates

--- build.config.js
+++ build.config.js
@@ -66,6 +66,7 @@ module.exports = {
       'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
       'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
       'vendor/angular-ui-router/release/angular-ui-router.js',
+      'vendor/angular-route/angular-route.min.js',
       'vendor/angular-ui-utils/modules/route/route.js'
     ],
     css: [
