b52bbdebb5d7cbd5efd67eaeb4d83e57db136f0f
Kelsey Hawley <kelseyr.hawley@gmail.com>
2015-06-17 23:12:45 -0700

    Adding the toolbar related code

---
+++ src/app/toolbar/toolbar.js
@@ -0,0 +1,16 @@
+/*
+ * ToolBar
+ * Created: 17 June 2015
+ * Author: Kelsey Hawley
+ */
+
+ angular.module( 'jsb.toolbar', [
+
+])
+
+.config(function config( $stateProvider ) {
+
+})
+
+.controller( 'toolbarController', function ToolbarController( $scope ) {
+});
